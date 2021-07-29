4b985dbc84aa531c41de33b7ddd2a965d2e0f77d
Florian Kairis <kairisflorian@gmail.com>
2021-07-29 15:54:03 +0200

    création des routes

--- backend/app.js
+++ backend/app.js
@@ -1,17 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
-
-const app = express();
 const mongoose = require('mongoose');
+const Sauce = require('./models/Sauce');
+const sauceRoutes = require('./routes/sauce');
 
+// Paramétrage et connexion à la base de donnée
 mongoose.connect('mongodb+srv://floriankairis:5JPyDvFlijgl1eSH@cluster0.y1z2b.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
   { useNewUrlParser: true,
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
+const app = express();
 
+// Paramétrage des headers
 app.use((req, res, next) => {
    res.setHeader('Access-Control-Allow-Origin', '*');
    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
@@ -21,33 +23,6 @@ app.use((req, res, next) => {
 
 app.use(bodyParser.json());
 
-app.post('/api/stuff', (req, res, next) => {
-   console.log(req.body);
-   res.status(201).json({
-     message: 'Objet créé !'
-   });
-});
-
-app.use('/api/stuff', (req, res, next) => {
-   const stuff = [
-     {
-       _id: 'oeihfzeoi',
-       title: 'Mon premier objet',
-       description: 'Les infos de mon premier objet',
-       imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-       price: 4900,
-       userId: 'qsomihvqios',
-     },
-     {
-       _id: 'oeihfzeomoihi',
-       title: 'Mon deuxième objet',
-       description: 'Les infos de mon deuxième objet',
-       imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-       price: 2900,
-       userId: 'qsomihvqios',
-     },
-   ];
-   res.status(200).json(stuff);
-});
+app.use('/api/stuff', sauceRoutes);
 
 module.exports = app;
